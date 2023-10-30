495fa4ce1cf5500e7f603ca1958b07d7f759e475
LeandroYasutake <leandroyasutake@gmail.com>
2023-10-30 20:04:12 -0300

    fix styles signin

--- src/components/signIn.js
+++ src/components/signIn.js
@@ -5,6 +5,11 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
 function SignIn() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Handle form submission logic here
+    };
+
     return (
         <Box className="signInBox"
             sx={{
@@ -18,36 +23,39 @@ function SignIn() {
         >
             <Box
                 sx={{
-                    width: '100%',
-                    maxWidth: '500px',
-                    backgroundColor: 'white',
+                    backgroundColor: '#e6e6e6', 
                     boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
                     borderRadius: '8px',
                     display: 'flex',
                     flexDirection: 'column',
                     alignItems: 'center',
                     padding: '2rem',
+                    border:1
                 }}
             >
                 <h1>Sign In</h1>
-                <TextField
-                    id="email"
-                    label="Email"
-                    variant="outlined"
-                    fullWidth
-                    margin="normal"
-                />
-                <TextField
-                    id="password"
-                    label="Password"
-                    type="password"
-                    variant="outlined"
-                    fullWidth
-                    margin="normal"
-                />
-                <Button variant="contained" fullWidth>
-                    Sign In
-                </Button>
+                <form onSubmit={handleSubmit} style={{ width: '100%', maxWidth: '400px' }}>
+                    <TextField
+                        id="email"
+                        label="Email"
+                        variant="outlined"
+                        fullWidth
+                        required
+                        margin="normal"
+                    />
+                    <TextField
+                        id="password"
+                        label="Password"
+                        type="password"
+                        variant="outlined"
+                        fullWidth
+                        margin="normal"
+                        required
+                    />
+                    <Button type="submit" variant="contained" fullWidth>
+                        Sign In
+                    </Button>
+                </form>
             </Box>
         </Box>
     );
